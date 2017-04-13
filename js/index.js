2c2a4b3705be02e0ac97f688cf701af5fca64781
chenming <294225707@qq.com>
2017-04-14 00:32:33 +0800

    添加一个使用创建水波纹的测试文件（注意事件执行的顺序）

--- js/index.js
+++ js/index.js
@@ -1,37 +1,43 @@
-var canvas = {},
+window.onload = function () {
+
+
+
+  var canvas = {},
     centerX = 0,
     centerY = 0,
     color = '',
     containers = document.getElementsByClassName('material-design')
-    context = {},
+  context = {},
     element = {},
     radius = 0,
 
     requestAnimFrame = function () {
       return (
-        window.requestAnimationFrame       || 
-        window.mozRequestAnimationFrame    || 
-        window.oRequestAnimationFrame      || 
-        window.msRequestAnimationFrame     || 
+        window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
         function (callback) {
           window.setTimeout(callback, 1000 / 60);
         }
       );
-    } (),
-      
+    }(),
+
     init = function () {
+      // 生成一个遮罩层在这个上面
       containers = Array.prototype.slice.call(containers);
       for (var i = 0; i < containers.length; i += 1) {
         canvas = document.createElement('canvas');
         canvas.addEventListener('click', press, false);
         containers[i].appendChild(canvas);
-        canvas.style.width ='100%';
-        canvas.style.height='100%';
-        canvas.width  = canvas.offsetWidth;
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
+        canvas.width = canvas.offsetWidth;
+        console.log(canvas.offsetHeight);
         canvas.height = canvas.offsetHeight;
       }
     },
-      
+    // 点击执行的事件
     press = function (event) {
       color = event.toElement.parentElement.dataset.color;
       element = event.toElement;
@@ -42,16 +48,17 @@ var canvas = {},
       context.clearRect(0, 0, element.width, element.height);
       draw();
     },
-      
+    // 每次重复执行每次添加6个像素 通过设置这个半径来改变速度
     draw = function () {
       context.beginPath();
       context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
       context.fillStyle = color;
       context.fill();
-      radius += 2;
-      if (radius < element.width) {
+      radius += 6;
+      if (radius < element.width * 1.5) {
         requestAnimFrame(draw);
       }
     };
 
-init();
+  init();
+}
